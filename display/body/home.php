0712c45cc900b1b5f11c9fc849405701d17ea8c4
James <james.luevano@gmail.com>
2014-10-03 16:50:24 -0700

    updated homepage

--- display/body/home.php
+++ display/body/home.php
@@ -1,2 +1,24 @@
-<h1>Welcome to the homepage!</h1>
-<p>This site is very dynamic and can be controlled from one file. Or perhaps it can be controlled by an admin's backend.</p>
+<h1>Software Engineering Club</h1>
+
+<p>The Software Engineering Club at Moreno Valley College will
+promote teamwork, teach industry standard collaboration techniques, and solidify the skills of
+computer science students by hosting periodic software development marathons in which the 
+goal will be to conceptualize, design, and produce a unique application as a group in a single</p>
+
+<p>In addition, a variety of software development topics ranging from basic to advanced
+will be discussed and demonstrated by qualified club members and/or advisors. These new 
+skills will be put to the test in fun-filled group exercises and competitions.</p>
+
+<p>All activities and applications will be documented on a club website as a showcase of the 
+computer science program at Moreno Valley College.</p>
+ <h3><b><u>Section B: Goals</u></b> – Specific goals of the club shall be:</h3>
+ <ol>
+ 	<li>To collectively learn new programming languages and techniques.</li>
+ 	<li>To collectively learn software development collaboration methods.</li>
+ 	<li>To challenge members with ambitious projects.</li>
+ 	<li>To develop and maintain a professional website.</li>
+ 	<li>To develop compelling computer applications.</li>
+ 	<li>To showcase the skills of the club.</li>
+ 	<li>To promote the computer science program at MVC.</li>
+
+ </ol>
