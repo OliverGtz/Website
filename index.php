1793422c520adf4995ae54e79091efc251b97522
aaron_b_clark <aaron_b_clark@yahoo.com>
2014-10-04 18:29:20 -0700

    Dynamic CSS, Pure-CSS, Header File
    Created function to pull css for body files.
    Linked to Pure-CSS files.
    Created Header file.
    Moved getNav() to header file.

--- index.php
+++ index.php
@@ -3,13 +3,18 @@
 <html>
 
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1"> <!--Pure-CSS mobile view-->
+	<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css"> <!--Pure-CSS main code-->
+	<link rel="stylesheet" href="display/body/stylesheets/inc_header.css"> <!--Header Stylesheet-->
+	<?php getCSS($pages, $pageNum); ?> <!--Body Stylesheet-->
+	
+
 	<title><?php getTitle($pageNum); ?></title>
   		   <?php getMeta(); ?>
+		   
 </head>
 <body>
-<ul>
-<?php getNav(); ?>
-</ul>
+<?php include ("display\body\inc_header.html"); ?>
 <?php getBody($pages, $pageNum); ?>
 </body>
 </html>
