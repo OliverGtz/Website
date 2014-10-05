04b24d37c04fd38f9fbc871b0e6e2b5aa7770769
aaron_b_clark <aaron_b_clark@yahoo.com>
2014-10-05 03:38:23 -0700

    Major Design Update
    Added content and partial design to 'home' and 'About' pages. Gives a
    potential feel for the site.
    Included javascript file for image reflection.
    Few other minor changes.
    Still needs lots of work!

--- display/body/about.php
+++ display/body/about.php
@@ -1,1 +1,31 @@
-<h1>About Us</h1>
+<div class="pure-g" id="about">
+	<div class="pure-u-3-24">
+	</div>
+	<div class="pure-u-18-24" id="centerdiv">
+		<center>
+		<strong>
+			<p style="margin-top: 50px;">The Software Engineering Club at Moreno Valley College
+			promotes teamwork, teaches industry standard collaboration techniques, and solidifies the skills of
+			computer science students by hosting periodic software development marathons in which the 
+			goal will be to conceptualize, design, and produce a unique application as a group in a single
+			session.</p>
+
+			<p>A variety of software development topics ranging from basic to advanced
+			will be presented by qualified speakers. These new 
+			skills will be put to the test in fun-filled group exercises and competitions.</p>
+			<p>Also, we have cake.</p>
+			<br />
+			<br />
+			<br />
+			<br />			
+			<hr>
+			<br />
+			<br />
+			<div id="languages-funny-div">
+			<img src="display/body/images/languages_funny.jpg" alt="languages_funny"
+				class="reflex itiltnone iopacity50">
+			</div>
+	</div>
+	<div class="pure-u-3-24">
+	</div>
+ </div>
--- index.php
+++ index.php
@@ -7,13 +7,16 @@
 	<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css"> <!--Pure-CSS main code-->
 	<link rel="stylesheet" href="display/body/stylesheets/inc_header.css"> <!--Header Stylesheet-->
 	<?php getCSS($pages, $pageNum); ?> <!--Body Stylesheet-->
+	<script type="text/javascript" src="includes/reflex.js"></script> <!--javascript for reflections-->
 	
 
 	<title><?php getTitle($pageNum); ?></title>
   		   <?php getMeta(); ?>
-		   
 </head>
-<body>
+<body style="height: 100%; 
+	background-image: url("http://cdn.wonderfulengineering.com/wp-content/uploads/2014/04/code-wallpaper-17.jpg");
+	background-repeat: repeat-y;
+	background-size: auto%;">
 <?php include ("display\body\inc_header.html"); ?>
 <?php getBody($pages, $pageNum); ?>
 </body>
