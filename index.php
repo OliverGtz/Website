1be9e25a0e25b2c00c941f258e87fe6af6040940
fenrirAB <zentetzuken9687@yahoo.com>
2014-10-05 21:02:47 -0700

    Move stylesheet directory + testing links
    I took the liberty to move the 'stylesheets' directory inside
    display\body\ into the parent directory. Judging from the 'index.php',
    we are pulling the stylesheets in a deeper hierarchy making the workflow
    inefficient (and tbh, a bit like a clown show). If there is a grand
    reasoning behind this, do let me know... PS. I also added a
    'project.php' and 'pageConstruction.css' to test out the images I have
    committed earlier. Request to test it out making sure I didnt break
    anything.

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
 
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1"> <!--Pure-CSS mobile view-->
-	<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css"> <!--Pure-CSS main code-->
-	<link rel="stylesheet" href="display/body/stylesheets/inc_header.css"> <!--Header Stylesheet-->
+	<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css"> <!--Pure-CSS main code-->
+	<link rel="stylesheet" type="text/css" href="display/body/stylesheets/inc_header.css"> <!--Header Stylesheet-->
+	<link rel="stylesheet" type="text/css" href="display/body/stylesheets/pageConstruction.css"> <!-- 404/underConstruction Stylesheet-->
 	<?php getCSS($pages, $pageNum); ?> <!--Body Stylesheet-->
 	<script type="text/javascript" src="includes/reflex.js"></script> <!--javascript for reflections-->
 	
