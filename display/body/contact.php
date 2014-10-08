f516659013b4e1fa74b561086740eb2365fd25ea
fenrirAB <zentetzuken9687@yahoo.com>
2014-10-07 23:52:58 -0700

    minor changes: colour schemes, font, etc.
    added a few style as a test for colour scheme. Tested out using custom
    font... anti-aliasing needs work. Formatted other stylesheets with tab.
    Also, make sure to read 1CSS_GUIDELINE.txt (feel free to add any more
    ideas that will help us code efficiently)

--- display/body/contact.php
+++ display/body/contact.php
@@ -6,26 +6,31 @@
 		<br />
 		<br />
 		<br />
-		<div style="width:100%;">
-			<a class="pure-button" id="link" href="https://github.com/MVC-SEC">Visit Our GitHub Repo</a>
-		</div>
-		<br />
-		<br />
-		<div style="width: 100%;">
-			<a class="pure-button" id="link" href="http://mvc.edu/">Moreno Valley College</a>
-		</div>
-		<br />
-		<br />
-		<div style="width: 100%">
-			<form action="MAILTO:aaron_b_clark@yahoo.com" method="post">
-			<input type="submit" value="E-mail The President" class="pure-button" id="link">
+		<div id="buttonDiv">
+			<form action="https://github.com/MVC-SEC" method="get">
+				<input class="pure-button" id="link" type="submit" value="Visit Our GitHub Repo" />
 			</form>
 		</div>
 		<br />
 		<br />
-		<div style="width: 100%">
+		<div id="buttonDiv">
+			<form action="http://mvc.edu/" method="get">
+				<input class="pure-button" id="link" type="submit" value="Moreno Valley College" />
+			</form>
+		</div>
+		<br />
+		<br />
+		<div id="buttonDiv">
+			<form action="MAILTO:aaron_b_clark@yahoo.com" method="post">
+				<input class="pure-button" id="link" type="submit" value="E-mail Our President" />
+			</form>
+		</div>
+		<br />
+		<br />
+		<div id="buttonDiv">
 			<form action="MAILTO:olig.333@gmail.com" method="post">
-			<input type="submit" value="E-mail The Webmaster" class="pure-button" id="link">
+				<input class="pure-button" id="link" type="submit" value="E-mail Our Webmaster" />
+			</form>
 		</div>		
 		<div style="height: 200px;">
 	</div>
