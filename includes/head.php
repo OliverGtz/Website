b6461196f2a2eac1d28d3990ef569251e7d8189d
aaron_b_clark <aaron_b_clark@yahoo.com>
2014-10-12 21:24:17 -0700

    Image Slider(alpha); Minor Modifications
    Added v0.4 of the image slider.
    Changed code in head.php for compatibility.
    Bug fix in about.php.
    Added images for slider testing.

--- includes/head.php
+++ includes/head.php
@@ -1,8 +1,8 @@
 <head>
  	<?php getMeta(); ?>
-	<link rel="stylesheet" type="text/css" href="<?=CSSPATH."main.css"?>"> <!-- MAIN STYLESHEET -->
-	<script type="text/javascript" src="<?=JAVASCRIPT."jquery-2.1.1.min.js"?>"></script>
-	<script language="javascript" type="text/javascript" src="<?=JAVASCRIPT."transparentHeader.js"?>"></script>
+	<link rel="stylesheet" type="text/css" href="<?php echo CSSPATH."main.css"?>"> <!-- MAIN STYLESHEET -->
+	<script type="text/javascript" src="<?php echo JAVASCRIPT."jquery-2.1.1.min.js"?>"></script>
+	<script language="javascript" type="text/javascript" src="<?php echo JAVASCRIPT."transparentHeader.js"?>"></script>
 
 	<title><?php getTitle($pageNum); ?></title>
   		  
