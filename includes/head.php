d13ab68500bf3d3b8de75c74d78e2bdba1137264
aaron_b_clark <aaron_b_clark@yahoo.com>
2014-10-26 03:09:40 -0700

    FlexSlider update
    Added new slider pugin.

--- includes/head.php
+++ includes/head.php
@@ -5,7 +5,8 @@
 
 	<link rel="stylesheet" type="text/css" href="<?php echo CSSPATH."mainHeader.css"?>"> <!-- MAINHEADER STYLESHEET: fenrirAB -->
 	<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css"> <!--Pure-CSS main code-->
-	<link rel="stylesheet" type="text/css" href="<?php echo CSSPATH."main.css"?>"> <!-- MAIN STYLESHEET -->
+	<link rel="stylesheet" type="text/css" href="<?php echo CSSPATH."main.css"?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo CSSPATH."flexslider.css"?>"> 	<!-- MAIN STYLESHEET -->
 
 	<!-- Start of responsive css from purecss.io -->
 	 	<!--[if lte IE 8]>
@@ -19,6 +20,13 @@
 	<script type="text/javascript" src="<?php echo JAVASCRIPT."jquery-2.1.1.min.js"?>"></script>
 	<script language="javascript" type="text/javascript" src="<?php echo JAVASCRIPT."jsFunctions.js"?>"></script>
 	<script language="javascript" type="text/javascript" src="<?php echo JAVASCRIPT."pageNotFound.js"?>"></script>	
+	<script language="javascript" type="text/javascript" src="<?php echo JAVASCRIPT."jquery.flexslider.js"?>"></script>	
+	<script language="javascript" type="text/javascript" src="<?php echo JAVASCRIPT."jquery.flexslider-min.js"?>"></script>	
+	<script type="text/javascript">
+		$(window).load(function() {
+			$('.flexslider').flexslider();
+		});
+	</script>
 
 	<title><?php getTitle($pageNum); ?></title>  		  
 </head>
